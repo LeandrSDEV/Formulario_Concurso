5bd338040ec52f375e115e06eab8cde9fb3a8c8a
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-12-29 17:00:39 -0300

    chore(EntitiAndPostgres): Criado a entidade 'Aprovado' e registrado o vinculo com o postgreSQL

---
+++ src/main/java/com/concurso/crud/model/Aprovado.java
@@ -0,0 +1,68 @@
+package com.concurso.crud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Aprovado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    @Column(length = 1000)
+    private String concursosAprovados;
+
+    private String imagemPath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getConcursosAprovados() {
+        return concursosAprovados;
+    }
+
+    public void setConcursosAprovados(String concursosAprovados) {
+        this.concursosAprovados = concursosAprovados;
+    }
+
+    public String getImagemPath() {
+        return imagemPath;
+    }
+
+    public void setImagemPath(String imagemPath) {
+        this.imagemPath = imagemPath;
+    }
+}
