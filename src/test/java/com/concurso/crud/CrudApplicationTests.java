b94e979f9319c6256734197c773eb7cad386dbbc
Leandro D'Jesus <crazyg4ng@gmail.com>
2025-12-29 16:39:17 -0300

    chore(Criação): Projeto iniciado pelo 'Spring Initializr' com SpringWev, SpringDataJPA,PostfreSQLDriver, Validation e Lombok

---
+++ src/test/java/com/concurso/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurso.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
